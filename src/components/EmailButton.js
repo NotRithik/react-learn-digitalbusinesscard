4fcd08c5d61da6cfd7ee00376001a433e4150344
NotRithik <rithikum123@gmail.com>
2022-02-06 20:35:58 +0530

    Create LinkedIn Button

--- src/components/EmailButton.js
+++ src/components/EmailButton.js
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 
+import emailIcon from '../icons/mail.png'
+
 export default function() {
     return(
         <button className="email-button body-contact-button">
-            <img src="./Components/Icons/mail.png" />
+            <img src={emailIcon}/>
             Email
         </button>
     )
