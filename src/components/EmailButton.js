6ca2ec85b445775de93adc78ca137def2594d2b9
NotRithik <rithikum123@gmail.com>
2022-02-06 20:34:26 +0530

    Initial Commit

---
+++ src/components/EmailButton.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+export default function() {
+    return(
+        <button className="email-button body-contact-button">
+            <img src="./Components/Icons/mail.png" />
+            Email
+        </button>
+    )
+}
