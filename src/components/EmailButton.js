47f77a5b65a6162d78d92cdb92c1c00a92b1631c
NotRithik <rithikum123@gmail.com>
2022-02-07 16:39:51 +0530

    Add email onClick hyperlink

--- src/components/EmailButton.js
+++ src/components/EmailButton.js
@@ -2,12 +2,17 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import emailIcon from '../icons/mail.png'
+let emailId = 'rithikum123@gmail.com'
 
 export default function() {
     return(
-        <button className="email-button body-contact-button">
+        <button onClick={sendEmail} className="email-button body-contact-button">
             <img src={emailIcon}/>
             Email
         </button>
     )
+
+    function sendEmail() {
+        window.location.assign("mailto:" + emailId)
+    }
 }
