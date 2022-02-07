95ca2e67364f64309ef957dbf0f4ea1e0961ecd9
NotRithik <rithikum123@gmail.com>
2022-02-07 16:46:47 +0530

    Add LinkedIn hyperlink

--- src/components/LinkedInButton.js
+++ src/components/LinkedInButton.js
@@ -2,12 +2,17 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import linkedinLogo from '../icons/linkedin.svg'
+let linkedinLink = 'https://www.linkedin.com/in/notrithik/'
 
-export default function() {
+export default function () {
     return (
-        <button className="linkedin-button body-contact-button">
-            <img src={linkedinLogo}/>
+        <button onClick={openLinkedinPage} className="linkedin-button body-contact-button">
+            <img src={linkedinLogo} />
             LinkedIn
         </button>
     )
+
+    function openLinkedinPage() {
+        window.open(linkedinLink, '_blank')
+    }
 }
