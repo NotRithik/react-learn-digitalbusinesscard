04c164939b327d53713b4c562abc19da78b5ff7f
NotRithik <rithikum123@gmail.com>
2022-02-07 16:20:12 +0530

    Change tab title and add warning heading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import './App.css';
 import DigitalBusinessCard from './components/DigitalBusinessCard';
 
-function App() {
-  return (
-    <DigitalBusinessCard />
-  );
+class App extends React.Component {
+  componentDidMount() {
+    document.title = 'Test App! Not really ready to be hosted :p'
+  }
+
+  render() {
+    return (
+      <div className="app-root" display="flex" flex-direction="column" align-content="center">
+        <h1>Test App! Not really ready to be hosted!</h1>
+        <DigitalBusinessCard />
+      </div>
+    );
+  }
 }
 
 export default App;
