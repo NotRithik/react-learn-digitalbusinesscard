157770801f5172c88b420f36a458c6d008024c66
NotRithik <rithikum123@gmail.com>
2022-02-07 19:59:17 +0530

    Wrap social media icons with anchor tag
    Wrap the social media buttons at the bottom of the page with anchor tag to make them clickable. The links open in new tabs when the icons are clicked

--- src/components/DigitalBusinessCard.js
+++ src/components/DigitalBusinessCard.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom'
-import React from 'react'
+import React, { useRef } from 'react'
 
 import EmailButton from './EmailButton'
 import LinkedInButton from './LinkedInButton'
@@ -11,41 +11,59 @@ import githubIcon from '../icons/github.svg'
 
 import rithiksPhoto from '../images/Rithik.jpg'
 
-function DigitalBusinessCard() {
-    return (
-        <div className="digital-business-card">
-            <div className="digital-business-card-image-wrapper">
-                <img src={rithiksPhoto} className="digital-business-card-image" />
-            </div>
+let twitterUrl = 'https://twitter.com/'
+let facebookUrl = 'https://facebook.com/'
+let instagramUrl = 'https://instagram.com/'
+let githubUrl = 'https://github.com/'
 
-            <div className="digital-business-card-body">
-                <h1 className="digital-business-card-body__name_heading">Rithik Kumar S</h1>
-                <h2 className="digital-business-card-body__description_heading">CS Student</h2>
-                <p className="digital-business-card__website">the-insomniacs.ddns.net</p>
-
-                <div className="digital-business-card-contact-buttons-wrapper">
-                    <EmailButton />
-                    <div className="digital-business-card-contact-buttons-wrapper-spacing-div" />
-                    <LinkedInButton />
+class DigitalBusinessCard extends React.Component {
+    render() {
+        return (
+            <div className="digital-business-card">
+                <div className="digital-business-card-image-wrapper">
+                    <img src={rithiksPhoto} className="digital-business-card-image" />
                 </div>
 
-                <div className="digital-business-card__body_text_wrapper">
-                    <h1 className="digital-business-card__about_heading digital-business-card__heading">About</h1>
-                    <p className="digital-business-card__about_text digital-business-card__text">I am a CS freshman at Ashoka University. I'm currently learning ReactJS and want to learn more about Web3, Blockchains and Entrepreneurship.</p>
+                <div className="digital-business-card-body">
+                    <h1 className="digital-business-card-body__name_heading">Rithik Kumar S</h1>
+                    <h2 className="digital-business-card-body__description_heading">CS Student</h2>
+                    <p className="digital-business-card__website">the-insomniacs.ddns.net</p>
 
-                    <h1 className="digital-business-card__interests_heading digital-business-card__heading">Interests</h1>
-                    <p className="digital-business-card__interests_text digital-business-card__text">Food, Traveling, Music.</p>
+                    <div className="digital-business-card-contact-buttons-wrapper">
+                        <EmailButton />
+                        <div className="digital-business-card-contact-buttons-wrapper-spacing-div" />
+                        <LinkedInButton />
+                    </div>
+
+                    <div className="digital-business-card__body_text_wrapper">
+                        <h1 className="digital-business-card__about_heading digital-business-card__heading">About</h1>
+                        <p className="digital-business-card__about_text digital-business-card__text">I am a CS freshman at Ashoka University. I'm currently learning ReactJS and want to learn more about Web3, Blockchains and Entrepreneurship.</p>
+
+                        <h1 className="digital-business-card__interests_heading digital-business-card__heading">Interests</h1>
+                        <p className="digital-business-card__interests_text digital-business-card__text">Food, Traveling, Music.</p>
+                    </div>
+                </div>
+
+                <div className="digital-business-card__footer">
+                    <a href={twitterUrl} className="footer_anchor" target="_blank">
+                        <img className="footer_icon" src={twitterIcon} />
+                    </a>
+
+                    <a href={facebookUrl} className="footer_anchor" target="_blank">
+                        <img className="footer_icon" src={facebookIcon} />
+                    </a>
+
+                    <a href={instagramUrl} className="footer_anchor" target="_blank">
+                        <img className="footer_icon" src={instagramIcon} />
+                    </a>
+                    
+                    <a href={githubUrl} className="footer_anchor" target="_blank">
+                        <img className="footer_icon" src={githubIcon} />
+                    </a>
                 </div>
             </div>
-
-            <div className="digital-business-card__footer">
-                <img className="footer_icon" src={twitterIcon} />
-                <img className="footer_icon" src={facebookIcon} />
-                <img className="footer_icon" src={instagramIcon} />
-                <img className="footer_icon" src={githubIcon} />
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default DigitalBusinessCard
